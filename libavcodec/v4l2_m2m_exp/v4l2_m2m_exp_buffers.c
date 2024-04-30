d113de6257768db672d3bd264389e7c46989e57f
AlexandrVolvenkin <aav-36@mail.ru>
2024-04-30 21:58:39 +0300

    If the input frame queue is processed, then we do not map the memory, since the imported buffer is of type: dma, with a physical address.

--- libavcodec/v4l2_m2m_exp/v4l2_m2m_exp_buffers.c
+++ libavcodec/v4l2_m2m_exp/v4l2_m2m_exp_buffers.c
@@ -527,23 +527,27 @@ int ff_v4l2_m2m_exp_buffer_initialize(V4L2Buffer* avbuf, int index)
     for (i = 0; i < avbuf->num_planes; i++) {
 
         avbuf->plane_info[i].bytesperline = V4L2_TYPE_IS_MULTIPLANAR(ctx->type) ?
-            ctx->format.fmt.pix_mp.plane_fmt[i].bytesperline :
-            ctx->format.fmt.pix.bytesperline;
+                                            ctx->format.fmt.pix_mp.plane_fmt[i].bytesperline :
+                                            ctx->format.fmt.pix.bytesperline;
 
         if (V4L2_TYPE_IS_MULTIPLANAR(ctx->type)) {
-            avbuf->plane_info[i].length = avbuf->buf.m.planes[i].length;
-            avbuf->plane_info[i].mm_addr = mmap(NULL, avbuf->buf.m.planes[i].length,
-                                           PROT_READ | PROT_WRITE, MAP_SHARED,
-                                           buf_to_m2mctx(avbuf)->fd, avbuf->buf.m.planes[i].m.mem_offset);
+            if (ctx->type == V4L2_BUF_TYPE_VIDEO_OUTPUT) {
+            } else {
+                avbuf->plane_info[i].length = avbuf->buf.m.planes[i].length;
+                avbuf->plane_info[i].mm_addr = mmap(NULL, avbuf->buf.m.planes[i].length,
+                                                    PROT_READ | PROT_WRITE, MAP_SHARED,
+                                                    buf_to_m2mctx(avbuf)->fd, avbuf->buf.m.planes[i].m.mem_offset);
+            }
         } else {
-            avbuf->plane_info[i].length = avbuf->buf.length;
-            avbuf->plane_info[i].mm_addr = mmap(NULL, avbuf->buf.length,
-                                          PROT_READ | PROT_WRITE, MAP_SHARED,
-                                          buf_to_m2mctx(avbuf)->fd, avbuf->buf.m.offset);
-        }
+            if (ctx->type == V4L2_BUF_TYPE_VIDEO_OUTPUT) {
 
-        if (avbuf->plane_info[i].mm_addr == MAP_FAILED)
-            return AVERROR(ENOMEM);
+            } else {
+                avbuf->plane_info[i].length = avbuf->buf.length;
+                avbuf->plane_info[i].mm_addr = mmap(NULL, avbuf->buf.length,
+                                                    PROT_READ | PROT_WRITE, MAP_SHARED,
+                                                    buf_to_m2mctx(avbuf)->fd, avbuf->buf.m.offset);
+            }
+        }
     }
 
     avbuf->status = V4L2BUF_AVAILABLE;
